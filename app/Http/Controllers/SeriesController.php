6ce483433a2cd372f12922d7d674aa17177f3345
Murillou <murillovinicius13@hotmail.com>
2025-03-19 18:47:34 -0300

    feat: criar controller e model das series

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series  = Serie::query()->orderBy('name')->get();
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serieName = $request->input('name');
+        $serie = new Serie();
+        $serie->name = $serieName;
+        $serie->save();
+
+        return redirect('/series');
+
+    }
+}
