9e57a7baa0f101779cab890ae83915d02682c076
Murillou <murillovinicius13@hotmail.com>
2025-03-22 01:40:37 -0300

    refactor: implementar layout de formulário e erro para ser utilizado entre as views

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,11 +1,4 @@
 <x-layout title="Nova Série">
-    <form action="{{ route('series.store') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-          <label for="name" class="form-label">Nome:</label>
-          <input type="text" id="name" name="name" class="form-control"/>
-        </div>
 
-        <button type="submit" class="btn btn-primary">Adicionar</button>
-      </form>
+  <x-series.form :action="route('series.store')" :name="old('name')" :update="false" />
 </x-layout>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -10,13 +10,18 @@
   <ul class="list-group">
     @foreach ($series as $serie) 
       <li class="list-group-item d-flex justify-content-between align-items-center">{{ $serie->name }}
-        <form action="{{ route('series.destroy', $serie->id) }}" method="post">
-          @csrf
-          @method('DELETE')
-          <button class="btn btn-danger btn-sm">
-            X
-          </button>
-        </form>
+        <span class="d-flex gap-3">
+            <button class="btn btn-warning btn-sm">
+              <a href="{{ route('series.edit', $serie->id ) }}">Editar</a>
+            </button>
+          <form action="{{ route('series.destroy', $serie->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger btn-sm">
+              X
+            </button>
+          </form>
+        </span>
       </li>
     @endforeach
   </ul>
