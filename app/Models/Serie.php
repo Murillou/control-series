e4bee5bd1ca0976aaa6ad37b59a1cf933ce1c803
Murillou <murillovinicius13@hotmail.com>
2025-03-22 01:39:09 -0300

    feat: criar model de Episode e Season e adicionar relações entre elas e Serie

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     protected $fillable = ['name'];
+
+
+    public function seasons()
+    {
+        return $this->hasMany(Season::class);
+    }
 }
