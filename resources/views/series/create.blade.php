037bf1961a559d09564dd80e3644f16edb3773d7
Murillou <murillovinicius13@hotmail.com>
2025-03-23 10:44:04 -0300

    refactor: alterar layouts das views

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,4 +1,27 @@
 <x-layout title="Nova Série">
+  <form action="{{ route('series.store') }}" method="post">
+        @csrf
+        
+        <div class="row mb-3">
+          <div class="col-8">
+            <label for="name" class="form-label">Nome:</label>
 
-  <x-series.form :action="route('series.store')" :name="old('name')" :update="false" />
+            <input type="text" autofocus id="name" name="name" class="form-control" value="{{ old('name') }}"/>
+          </div>
+
+          <div class="col-2">
+            <label for="seasons" class="form-label">N° Temporadas:</label>
+
+            <input type="text" id="seasons" name="seasons" class="form-control" value="{{ old('seasons') }}"/>
+          </div>
+
+          <div class="col-2">
+            <label for="episode" class="form-label">Eps / Temporada:</label>
+
+            <input type="text" id="episode" name="episode" class="form-control" value="{{ old('episode') }}"/>
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+  </form>
 </x-layout>
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="Editar série {{ $series->name }}">
+<x-layout title="Editar série {!! $series->name !!}">
   <x-series.form :action="route('series.update', $series->id)"  :name="$series->name" :update="true"/>
 
 </x-layout>
