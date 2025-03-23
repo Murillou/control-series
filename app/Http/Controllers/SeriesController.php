3f63cd01e167362a04fcb143be44f87fd39902c5
Murillou <murillovinicius13@hotmail.com>
2025-03-23 10:43:22 -0300

    refactor: alterar nomes de métodos/classes

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -27,6 +29,26 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $serie = Series::create($request->all());
+        $seasons = [];
+        for ($i = 1; $i <= $request->seasons; $i++) {
+            $seasons[] =[
+                'series_id' => $serie->id,
+                'number' => $i
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($serie->seasons as $season)  {
+            for ($j = 1; $j <= $request->episode; $j++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
+            }
+        }
+        Episode::insert($episodes);
+
 
         return to_route('series.index')->with('success.message', "Série {$serie->name} adicionada com sucesso.");
 
