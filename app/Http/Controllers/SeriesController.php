9bdd3828e8d74c3b7f506b39c187c4ac3a2e553c
Murillou <murillovinicius13@hotmail.com>
2025-03-24 03:11:44 -0300

    feat: implementar repositório para cuidar das injeções SQL e implementando provider

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $repository)
+    {
+
+    }
     public function index(Request $request)
     {
         $series  = Series::with(['seasons'])->get();
@@ -28,30 +30,9 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Series::create($request->all());
-        $seasons = [];
-        for ($i = 1; $i <= $request->seasons; $i++) {
-            $seasons[] =[
-                'series_id' => $serie->id,
-                'number' => $i
-            ];
-        }
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach ($serie->seasons as $season)  {
-            for ($j = 1; $j <= $request->episode; $j++) {
-                $episodes[] = [
-                    'season_id' => $season->id,
-                    'number' => $j
-                ];
-            }
-        }
-        Episode::insert($episodes);
-
+        $serie = $this->repository->add($request);
 
         return to_route('series.index')->with('success.message', "Série {$serie->name} adicionada com sucesso.");
-
     }
 
     public function edit(Series $series)
