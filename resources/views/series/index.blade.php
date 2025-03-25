c1d2a5433372c8327d252e8655646f165756d327
Murillou <murillovinicius13@hotmail.com>
2025-03-25 17:14:37 -0300

    refactor: implementar mensagem de erro no layout padrão

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,11 +1,5 @@
-<x-layout title="Séries">
+<x-layout title="Séries" :success-message="$successMessage>
   <a href="{{ route('series.create') }}" class="btn btn-dark mb-1"> Adicionar </a>
-
-  @isset($successMessage)
-  <div class="alert alert-success">
-    {{ $successMessage }}
-  </div>
-  @endisset
 
   <ul class="list-group">
     @foreach ($series as $serie) 
