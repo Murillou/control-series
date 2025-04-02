ca02b6e5f7ac63c306cb6cbb620531a2e212aad9
Murillou <murillovinicius13@hotmail.com>
2025-04-02 18:33:17 -0300

    feat: implementar rota de login e middleware de autenticação com sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/series', \App\Http\Controllers\Api\ApiSeriesController::class);
+//Route::apiResource('/series', \App\Http\Controllers\Api\ApiSeriesController::class);
+//Route::apiResource('/seasons', \App\Http\Controllers\Api\ApiSeasonsController::class);
+//Route::apiResource('/episodes', \App\Http\Controllers\Api\ApiEpisodesController::class);
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', \App\Http\Controllers\Api\ApiSeriesController::class);
+
+    Route::apiResource('/series/{series}/seasons', \App\Http\Controllers\Api\ApiSeasonsController::class);
+
+    Route::apiResource('/series/{series}/episodes', \App\Http\Controllers\Api\ApiEpisodesController::class);
+    Route::apiResource('/episodes', \App\Http\Controllers\Api\ApiEpisodesController::class);
+});
+
+
+Route::apiResource('/login', \App\Http\Controllers\Api\ApiLoginController::class);
