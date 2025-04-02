1599c87493e86e87bbc00ea887a8c08c6d762b5c
Murillou <murillovinicius13@hotmail.com>
2025-04-02 15:53:39 -0300

    feat: implementar métodos de rota update e delete

--- app/Http/Controllers/Api/ApiSeriesController.php
+++ app/Http/Controllers/Api/ApiSeriesController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class ApiSeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index(Request $request)
     {
         return Series::all();
@@ -16,6 +21,27 @@ class ApiSeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        return response()->json(Series::create($request->all()), 201);
+        return response()
+            ->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(Series $series)
+    {
+        return $series;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(Series $series)
+    {
+        $series->delete();
+
+        return response()->noContent();
     }
 }
